5f7be8d7889eaf6ded7384742b1d691f6b0b616e
Celso Junior <celsoestercio.jr@gmail.com>
2021-03-15 22:48:39 -0300

    first commit, create project

---
+++ src/test/java/com/br/projectorder/ProjectOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projectorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
