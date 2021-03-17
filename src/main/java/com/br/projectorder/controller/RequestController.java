2e9996c2cce56b28407bf8de61259d313aceb87a
Celso Junior <celsoestercio.jr@gmail.com>
2021-03-16 22:24:44 -0300

    feat: implements request save method

--- src/main/java/com/br/projectorder/controller/RequestController.java
+++ src/main/java/com/br/projectorder/controller/RequestController.java
@@ -1,24 +1,31 @@
 package com.br.projectorder.controller;
 
+import com.br.projectorder.domain.orm.Customer;
 import com.br.projectorder.domain.orm.Request;
+import com.br.projectorder.service.CustomerService;
 import com.br.projectorder.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/request")
 public class RequestController {
 
     @Autowired
-    private RequestService service;
+    private RequestService requestService;
+
+    @Autowired
+    private CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<Request> save(@RequestBody Request request) {
-
-        return ResponseEntity.ok(service.save(request));
+    public ResponseEntity<Request> save(@RequestBody @Valid Request request) {
+        return ResponseEntity.ok(requestService.save(request));
     }
 }
--- src/main/java/com/br/projectorder/service/RequestService.java
+++ src/main/java/com/br/projectorder/service/RequestService.java
@@ -1,9 +1,20 @@
 package com.br.projectorder.service;
 
+import com.br.projectorder.domain.orm.Customer;
+import com.br.projectorder.domain.orm.Product;
 import com.br.projectorder.domain.orm.Request;
+import com.br.projectorder.domain.repository.CustomerRepository;
+import com.br.projectorder.domain.repository.ProductRepository;
 import com.br.projectorder.domain.repository.RequestRepository;
+import com.br.projectorder.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class RequestService {
@@ -11,7 +22,27 @@ public class RequestService {
     @Autowired
     private RequestRepository requestRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
     public Request save(Request request) {
+        Optional<Customer> customer = customerRepository.findById(request.getCustomer().getId());
+        if(!customer.isPresent()) {
+            throw new NotFoundException("Cliente não encontrado.");
+        }
+        List<Product> products = new ArrayList<>();
+        request.getProducts().forEach(it -> {
+            Optional<Product> product = productRepository.findById(it.getId());
+            if(!product.isPresent()) {
+                throw new NotFoundException("Produto "+it.getId()+" não encontrado.");
+            }
+            products.add(product.get());
+        });
+        long totalPrice = products.stream().mapToLong(Product::getPrice).sum();
+        request.setFinalPrice(totalPrice);
         return requestRepository.save(request);
     }
 }
